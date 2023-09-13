31d258f1419264190472aa99c9318195c0050b35
Arshvir Singh <arshvirsignh16@gmail.com>
2023-09-12 20:08:38 -0700

    Complete quiz with test cases.

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -11,7 +11,16 @@ def bound_to_180(angle):
     Returns:
         float: The bounded angle in degrees.
     """
-    return 0
+    if angle > 180:
+        while angle > 180:
+            angle -= 360
+        return angle
+    elif angle <  - 180:
+        while angle < -180:
+            angle += 360
+        return angle
+    else:
+        return angle
 
 
 def is_angle_between(first_angle, middle_angle, second_angle):
@@ -29,4 +38,46 @@ def is_angle_between(first_angle, middle_angle, second_angle):
     Returns:
         bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
     """
-    return True
+
+    # Bound all angles
+    first_angle = bound_to_180(first_angle)
+    middle_angle = bound_to_180(middle_angle)
+    second_angle = bound_to_180(second_angle)
+
+    # Ideal Scenario: first and second angles are either in the top or bottom half
+
+    # First case: first and middle angle in first two quadrants 
+    if first_angle >= 0 and second_angle >= 0:
+        if first_angle < second_angle:
+            return first_angle < middle_angle < second_angle
+        else:
+            return second_angle < middle_angle < first_angle 
+
+    # Second Case: first and middle angle in last two quadrants
+    elif first_angle < 0 and second_angle < 0:
+        if first_angle < second_angle:
+            return first_angle < middle_angle < second_angle
+        else:
+            return second_angle < middle_angle < first_angle
+
+    # Unideal Scenario: First angle and second angle are split between top and bottom half
+    # Would have to check for reflex angle
+
+    # First case: first angle is in top half while second angle is on the bottom half
+    if first_angle > second_angle:
+        angle_between = first_angle - second_angle
+
+        if angle_between < 180:
+            return second_angle < middle_angle < first_angle
+        else:
+            return first_angle < middle_angle < (second_angle + 360)
+    
+    # Second case: first angle is in bottom half while second angle is on top  
+    if second_angle > first_angle:
+        angle_between = second_angle - first_angle
+        if angle_between < 180:
+            return first_angle < middle_angle < second_angle
+        else:
+            return second_angle < middle_angle + 360 < (first_angle + 360)
+
+
--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -6,10 +6,58 @@ from standard_calc import bound_to_180, is_angle_between
 
 def test_bound_basic1():
     assert bound_to_180(0) == 0
-
+def test_bound_basic2():
+    assert bound_to_180(135) == 135
+def test_bound_basic3():
+    assert bound_to_180(-181) == 179
+def test_bound_basic4():
+    assert bound_to_180(700) == -20
+def test_bound_basic5():
+    assert bound_to_180(-700) == 20
+ 
 
 """ Tests for is_angle_between() """
 
 
 def test_between_basic1():
-    assert is_angle_between(0, 1, 2)
+    assert is_angle_between(0, 1, 2) == True
+
+def test_between_basic2():
+    assert is_angle_between(15,90,150) == True
+
+def test_between_basic3():
+    assert is_angle_between(190,270,350) == True
+
+def test_between1():
+    assert is_angle_between(30,90,250) == False
+
+def test_between2():
+    assert is_angle_between(30,280,250) == True
+
+def test_between3():
+    assert is_angle_between(1,90,181) == True
+
+def test_between4():
+    assert is_angle_between(2,90,181) == True
+
+def test_between5():
+    assert is_angle_between(2,190,181) == False
+
+def test_between6():
+    assert is_angle_between(-15,90,179) == False
+
+def test_between7():
+    assert is_angle_between(-15,270,179) == True
+
+def test_between8():
+    assert is_angle_between(-15,90,110)== True
+
+def test_between9():
+    assert is_angle_between(2,90,190)== False
+
+def test_between10():
+    assert is_angle_between(2,270,190)== True
+
+
+
+
